443fcc1cbfabfecc662cc5a952f293cb348c0770
Andrew Marrone <github@andrewmarrone.com>
2014-02-28 12:29:19 -0800

    About, Help, and Home pages implemented with tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,10 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Welcome!')
     end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
   end
 
   describe "Help page" do
@@ -14,12 +18,20 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
   end
 
   describe "About page" do
     it  "should have the content 'About'" do
       visit '/static_pages/about'
       expect(page).to have_content('About')
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About")
     end
   end
 
