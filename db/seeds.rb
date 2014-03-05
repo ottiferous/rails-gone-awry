2ceb153166840d37c83cf35ee367d7f385bcc098
Andrew Marrone <github@andrewmarrone.com>
2014-03-04 16:03:57 -0800

    Chapter 6 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(name: "Testoferous",
+            email: "testoferous@domain.com",
+            password: "foobar",
+            password_confirmation: "foobar")
