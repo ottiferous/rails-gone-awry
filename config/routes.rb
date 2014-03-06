e11ee1088d708c1206358c148b69ec8073de4e07
Andrew Marrone <github@andrewmarrone.com>
2014-03-06 12:21:49 -0800

    signin paths and authentiate tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ SampleApp::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home'
 
-  match '/signup',  to: 'users#new',  via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'get'
 
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -20,6 +20,11 @@ describe "Authentication" do
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error') }
     end
+
+    describe "after visiting another page" do
+      before { click_link "Home" }
+      it { should_not hve_selector('div.alert.alert-error') }
+    end
   end
 
   describe "with valid information" do
